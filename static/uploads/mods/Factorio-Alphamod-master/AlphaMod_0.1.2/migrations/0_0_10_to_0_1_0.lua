eb6531ec0c0277ace66a31b43b8894c5df6bda4e
nekanayaka <nekanayaka@hollandcollege.com>
2016-04-08 12:16:32 +0000

    taking out the all the mod folders

--- static/uploads/mods/Factorio-Alphamod-master/AlphaMod_0.1.2/migrations/0_0_10_to_0_1_0.lua
+++
@@ -1,4 +0,0 @@
-for i, player in pairs(game.players) do
-    player.force.resetrecipes()
-    player.force.resettechnologies()
-end
--- static/uploads/mods/FactorioBasics-Core_0.1.3/data.lua
+++
@@ -1,6 +0,0 @@
-require("prototypes.item.item-groups")
-require("prototypes.item.items")
-
-require("prototypes.recipe.recipes")
-
-require("prototypes.technology.technology")
